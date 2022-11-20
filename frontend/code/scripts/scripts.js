4e483fe9b00e68615984a805410264e83cf35a53
Lukas <orpiczymejl@gmail.com>
2022-11-20 04:00:11 +0100

    Adds base frontend

---
+++ frontend/code/scripts/scripts.js
@@ -0,0 +1,13 @@
+
+const baseUrl = "host:port";
+
+const sendDesiredStateBtn = document.getElementById("update-device-state-btn");
+let deviceData = {};
+
+
+const updateDeviceData = async () => {
+    console.log(`Updating data...${i}`);
+    i ++;
+}
+
+setInterval(updateData,500);
